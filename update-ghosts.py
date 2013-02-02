3860d6827e74d558fc31bba0ac199a65fcc289f3
Peter Johnson <pajohnson@email.wm.edu>
2013-02-01 20:37:38 -0500

    Most of the program is complete, and update_ghosts() works correctly. Now to handle command line arguments.

--- update-ghosts.py
+++ update-ghosts.py
@@ -12,17 +12,64 @@ login_url = base_url + '/Account/Login'
 ghosts_url = base_url + '/Rider/Ghost'
 
 
-def find_links(link_name, text):
+# Searches for links with the given name.
+def find_links(link_name, html):
     pattern = r'href="([^"]+)">{}</a>'.format(link_name)
-    links = re.findall(pattern, text)
+    links = re.findall(pattern, html)
     return [base_url + link for link in links]
 
+# Finds the name of the course from its leaderboard.
+def find_leaderboard_title(html):
+    pattern = r'<h2>([^<]+)</h2>'
+    return re.findall(pattern, html)[0].strip()
 
+# Finds the first place person on a leaderboard
+def find_first_place(html):
+    pattern = r'1\s*</td>\s*<td[^>]*>\s*<a href="([^"]+)">([^>]*)</a>'
+    return re.findall(pattern, html)[0]
+
+# Finds the link to challenge a ghost.
+def find_challenge_url(html):
+    pattern =r'action="([^"]+)"'
+    return re.findall(pattern, html)[0]
+
+
+# Updates a ghost given it's change url.
 def update_ghost(url):
-    print url
+    global cookies, all_time, local
+
+    # Retrieve the leaderboard.
+    html = requests.get(url, cookies=cookies).text
+    url = find_links('My Facility' if local else 'Global', html)[0]
+    if all_time:
+        url += '&seasonId=1'
+    leaderboard = requests.get(url, cookies=cookies).text
+    title = find_leaderboard_title(leaderboard)
+
+    # Look at the first place person on the leaderboard.
+    first_place =  find_first_place(leaderboard)
+    print '{} Leaderboard:'.format(title).ljust(30),
+    if not first_place:
+        print 'No ghosts -- the leaderboard is empty!'
+    else:
+        ghost_url, name = first_place
+        if '(Ghost)' in name:
+            mesg = 'Ghost is up to date:'
+        else:
+            # Navigate to the ghost's page.
+            ghost_url = base_url + ghost_url
+            ghost_info = requests.get(ghost_url, cookies=cookies).text
+            # Actually change the ghost.
+            challenge_url = base_url + find_challenge_url(ghost_info)
+            requests.post(challenge_url, headers= \
+                    { 'content-length': '0' }, cookies=cookies).text
+            mesg = 'Ghost changed to:'
+        print '{} {}'.format(mesg.ljust(20), name)
 
 
 if __name__ == '__main__':
+    local = True
+    all_time = True
     _, expresso_id, password = sys.argv
 
     # Attempt to login.
